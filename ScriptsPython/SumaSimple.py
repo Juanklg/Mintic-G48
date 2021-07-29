a1dc80a0db63d32b7a5549a5f5ed91193ca95f2a
DESKTOP-QI3AGC3\Juan <juanklg25@gmail.com>
2021-07-29 13:36:12 -0500

    Script con ingreso teclado en SumaSimple

---
+++ ScriptsPython/SumaSimple.py
@@ -0,0 +1,13 @@
+print("Aca empieza mi codigo de suma simple------------------")
+#asi se crea una variable numerica
+numero1 = 7
+#asi se imprime un texto con una variable
+print("El primer numero constante es:"+str(numero1))
+#asi se asigna a una variable una entrada de teclado
+numero2 = input("Ingrese el segundo numero : ")
+#aca se hace la operacion de la suma simple
+resultado = numero1 + int(numero2)
+#asi se crea una variable tipo string
+nombre = "Suma Simple"
+print("El resultado de "+nombre+" es:"+str(resultado))
+print("Aca termina mi codigo de suma simple--------------------")
