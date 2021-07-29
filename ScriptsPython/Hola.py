98648b9aa700d688c28fed951c51fa7aebb8e388
DESKTOP-QI3AGC3\Juan <juanklg25@gmail.com>
2021-07-29 12:54:26 -0500

    Crear documentacion de inicio con jupyter y python

---
+++ ScriptsPython/Hola.py
@@ -0,0 +1,13 @@
+print("Aca empieza mi codigo--------------------------------------")
+#asi se crea una variable numerica
+numero = 1
+#asi se crea una variable tipo string
+nombre = "Juan"
+#asi se imprime un texto con una variable
+print("Hola "+nombre)
+#asi se imprime en consola o en terminal
+print(numero)
+#asi se hace un incremento de una variable
+numero = numero + 1
+print(numero)
+print("Aca termina mi codigo--------------------------------------")
